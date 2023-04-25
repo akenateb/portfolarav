eab3f554ea74827eb177235e35994be49eada422
4k3n4t3b <microhtc@gmail.com>
2023-04-25 13:03:12 +0100

    Subiendo modelos (article y category), controladores y migraciones de los mismos

---
+++ app/Models/Article.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Models;
+
+use Carbon\Carbon;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Support\Str;
+
+class Article extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        "title","content","category_id","user_id"
+    ];
+
+    protected $perPage = 5;
+
+    protected static function boot(){
+        parent::boot();
+        self::creating(function (Article $article){
+            $article->user_id = auth()->id();
+        });
+    }
+
+    public function user(): BelongsTo{
+        return $this->belongsTo(User::class);
+    }
+
+    public function category(): BelongsTo{
+        return $this->belongsTo(Category::class);
+    }
+
+    public function getCreateAtFormattedAttribute():string{
+        return Carbon::parse($this->created_at)->format("d-m-Y H:i");
+    }
+
+    public function getExcerptAttribute():string{
+        return Str::excerpt($this->content);
+    }
+}
