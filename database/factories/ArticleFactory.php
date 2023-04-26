2fd063ab562f645d504998fc92e174b4c16cd9b4
4k3n4t3b <microhtc@gmail.com>
2023-04-26 10:38:52 +0100

    Configurando el controlador de articulos, creando faker factory y seeders con categorias y usuarios.

---
+++ database/factories/ArticleFactory.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Category;
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Article>
+ */
+class ArticleFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'title' => fake()->name(),
+            'content' => fake()->unique()->safeEmail(),
+            'category_id'=>Category::all()->random(1)->first()->id,
+            'user_id'=>User::all()->random(1)->first()->id,
+            'created_at'=>now(),
+        ];
+    }
+}
